a71e3b6ca69ecf073b5dd5281caee3ed0675d7ef
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-10-02 19:42:07 +0200

    fixed .env and show if domain can't be accessed

--- src/Controller/CoinsController.php
+++ src/Controller/CoinsController.php
@@ -107,69 +107,81 @@ class CoinsController extends AppController {
 
     public function sync() {
 
+        $return      = [];
         $url         = "https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=EUR";
-        $json        = json_decode( file_get_contents( $url ) );
-        $touched_ids = [];
+        $json_string = file_get_contents( $url );
+        if( $json_string ) :
 
-        if( !empty( $json ) ) {
-            foreach( $json->coins as $entry ) {
+            $json        = json_decode( $json_string );
+            $touched_ids = [];
 
-                $data = [
-                    'name'            => $entry->name,
-                    'ext_ident'       => $entry->id,
-                    'symbol'          => $entry->symbol,
-                    'rank'            => $entry->rank,
-                    'price'           => $entry->price,
-                    'volume'          => $entry->volume,
-                    'marketCap'       => $entry->marketCap,
-                    'availableSupply' => $entry->availableSupply,
-                    'totalSupply'     => $entry->totalSupply,
-                    'priceChange1h'   => $entry->priceChange1h,
-                    'priceChange1d'   => $entry->priceChange1d,
-                    'priceChange1w'   => $entry->priceChange1w,
-                    'websiteUrl'      => $entry->websiteUrl
-                ];
+            if( !empty( $json ) ) {
+                foreach( $json->coins as $entry ) {
 
-                $entity = false;
-
-                $result = $this->Coins->find()
-                    ->where( [ 'ext_ident' => $entry->id ] )
-                    ->toArray();
-
-                // Update or create depending if the entity is already in DB
-                if( sizeof( $result ) == 1 ) {
-                    $entity = $result[0];
-                } elseif( sizeof( $result ) == 0 ) {
-                    $entity = $this->Coins->newEmptyEntity();
-                }
-                $entity = $this->Coins->patchEntity( $entity, $data );
-
-                if( $this->Coins->save( $entity ) ) {
-                    $result['success'][] = $entity->id;
-                } else {
-                    $result['error'][] = [
-                        'id'    => $entity->id,
-                        'error' => $entity->getErrors()
+                    $data = [
+                        'name'            => $entry->name,
+                        'ext_ident'       => $entry->id,
+                        'symbol'          => $entry->symbol,
+                        'rank'            => $entry->rank,
+                        'price'           => $entry->price,
+                        'volume'          => $entry->volume,
+                        'marketCap'       => $entry->marketCap,
+                        'availableSupply' => $entry->availableSupply,
+                        'totalSupply'     => $entry->totalSupply,
+                        'priceChange1h'   => $entry->priceChange1h,
+                        'priceChange1d'   => $entry->priceChange1d,
+                        'priceChange1w'   => $entry->priceChange1w,
+                        'websiteUrl'      => $entry->websiteUrl
                     ];
+
+                    $entity = false;
+
+                    $result = $this->Coins->find()
+                        ->where( [ 'ext_ident' => $entry->id ] )
+                        ->toArray();
+
+                    // Update or create depending if the entity is already in DB
+                    if( sizeof( $result ) == 1 ) {
+                        $entity = $result[0];
+                    } elseif( sizeof( $result ) == 0 ) {
+                        $entity = $this->Coins->newEmptyEntity();
+                    }
+                    $entity = $this->Coins->patchEntity( $entity, $data );
+
+                    if( $this->Coins->save( $entity ) ) {
+                        $return['success'][] = $entity->id;
+                    } else {
+                        $return['error'][] = [
+                            'id'    => $entity->id,
+                            'error' => $entity->getErrors()
+                        ];
+                    }
+
+                    $touched_ids[] = $entry->id;
+
                 }
-
-                $touched_ids[] = $entry->id;
-
             }
-        }
 
-        if( !empty( $touched_ids ) ):
-            $db_entries = $this->Coins->find( 'list', [
-                'valueField' => 'ext_ident'
-            ] )->toArray();
-            $diff       = array_diff( $db_entries, $touched_ids );
-            if( !empty( $diff ) ):
-                $this->Coins->deleteAll( [ 'ext_ident IN' => $diff ] );
+            if( !empty( $touched_ids ) ):
+                $db_entries = $this->Coins->find( 'list', [
+                    'valueField' => 'ext_ident'
+                ] )
+                    ->toArray();
+                $diff       = array_diff( $db_entries, $touched_ids );
+                if( !empty( $diff ) ):
+                    $this->Coins->deleteAll( [ 'ext_ident IN' => $diff ] );
+                endif;
             endif;
-        endif;
 
-        $this->Flash->success( 'Sync is done!' );
-        $this->redirect( [ 'action' => 'index' ] );
+            $this->Flash->success( 'Sync is done!' );
+            $this->redirect( [ 'action' => 'index' ] );
+
+        else:
+
+            $this->Flash->error( 'Coin API seems to be down!' );
+            $this->redirect( [ 'action' => 'index' ] );
+
+        endif;
 
     }
 
